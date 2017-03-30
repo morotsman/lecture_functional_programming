2b164641637aca8d7b11e9dc3d82ef11bdeb7bbf
morotsman <leopold.niklas@gmail.com>
2017-03-30 17:09:37 +0200

    more

--- recursion/assignments/factorial.js
+++ recursion/assignments/factorial.js
@@ -1,5 +1,13 @@
 "use strict";
 
+/*
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+
+The value of 0! is 1, according to the convention for an empty product.
+5!=5*4*3*2*1=120. 
+
+*/
+
 const factorial = number => {
 
 }
--- recursion/assignments/fibonacci.js
+++ recursion/assignments/fibonacci.js
@@ -1,4 +1,11 @@
 
+/*
+
+By definition, the first two numbers in the Fibonacci sequence are either 1 and 1, or 0 and 1, depending on the chosen starting point of the sequence, and each subsequent number is the sum of the previous two.
+
+In mathematics, the Fibonacci numbers are the numbers in the following integer sequence, called the Fibonacci sequence, and characterized by the fact that every number after the first two is the sum of the two preceding ones:
+*/
+
 const fibonacci = number => {
 
 };
