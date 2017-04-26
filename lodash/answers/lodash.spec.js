03f7cb8ff99005b90b880623e12413b5a6fc3189
morotsman <leopold.niklas@gmail.com>
2017-04-26 16:15:07 +0200

    lodash

--- lodash/answers/lodash.spec.js
+++ lodash/answers/lodash.spec.js
@@ -2,30 +2,25 @@ describe('lodash', function () {
 	
 	
 	
-    
-
-	
-	
-    /**************Assignments use lodash in all assignments**********************/
-	
     //Documentaion at: https://lodash.com/docs/4.17.4
     
 	
-	//map
+	/***********************************map***************************************/
+	
     it("convert list of squares to triangles", function(){
         const squares = [];
         
-		const actual = _(squares).map(square => "triangle");
+		const actual = _(squares).map(square => "triangle").value();
 		
-		expect(_.toArray(actual)).toEqual([]);
+		expect(actual).toEqual([]);
     });		
 	
     it("convert list of squares to triangles", function(){
         const squares = ["square","square","square"];
         
-		const actual = _(squares).map(square => "triangle");
+		const actual = _(squares).map(square => "triangle").value();
 		
-		expect(_.toArray(actual)).toEqual(["triangle","triangle", "triangle"]);
+		expect(actual).toEqual(["triangle","triangle", "triangle"]);
     });	
 
 	
@@ -33,32 +28,39 @@ describe('lodash', function () {
 	//map assignments
     it("add 1 to each number in the list", function(){
         const numbers = [1,2,3];
-        const actual = _(numbers).map(number => number + 1);
-		expect(_.toArray(actual)).toEqual([2,3,4]);
+
+        const actual = _(numbers).map(number => number + 1).value();
+		
+		expect(actual).toEqual([2,3,4]);
     });	  
 	
     it("get length of each word in the list", function(){
         const words = ["a", "list", "of", "words"];
-        const actual = _(words).map(word => word.length);
-		expect(_.toArray(actual)).toEqual([1,4,2,5]);
+		
+        const actual = _(words).map(word => word.length).value();
+		
+		expect(actual).toEqual([1,4,2,5]);
     });		
     	
     it("get the first name of each person", function(){
         const persons = [{firstName:"Kalle", lastName:"Anka"},{firstName:"Musse", lastName:"Pigg"},{firstName:"Arne", lastName:"Anka"}];
-        const actual = _(persons).map(person => person.firstName);
-		expect(_.toArray(actual)).toEqual(["Kalle", "Musse", "Arne"]);
+		
+        const actual = _(persons).map(person => person.firstName).value();
+		
+		expect(actual).toEqual(["Kalle", "Musse", "Arne"]);
     });		
 
 
 	
 	
-	//filter
+	/**************************************filter*******************************/
+	
     it("keep all the triangles", function(){
         const shapes = [];
         
-		const actual = _(shapes).filter(shape => shape === "triangle");
+		const actual = _(shapes).filter(shape => shape === "triangle").value();
 		
-		expect(_.toArray(actual)).toEqual([]);
+		expect(actual).toEqual([]);
     });	
 	
 	const isTriangle = shape => shape === "triangle";
@@ -66,19 +68,19 @@ describe('lodash', function () {
     it("keep all the triangles", function(){
         const shapes = ["square","triangle","square", "triangle"];
         
-		const actual1 = _(shapes).filter(shape => shape === "triangle");
-		const actual2 = _(shapes).filter(isTriangle);
+		const actual1 = _(shapes).filter(shape => shape === "triangle").value();
+		const actual2 = _(shapes).filter(isTriangle).value();
 		
-		expect(_.toArray(actual1)).toEqual(["triangle","triangle"]);
-		expect(_.toArray(actual2)).toEqual(["triangle","triangle"]);
+		expect(actual1).toEqual(["triangle","triangle"]);
+		expect(actual2).toEqual(["triangle","triangle"]);
     });	
 
     it("keep all the stars", function(){
         const shapes = ["square","triangle","square", "triangle"];
         
-		const actual = _(shapes).filter(shape => shape === "star");
+		const actual = _(shapes).filter(shape => shape === "star").value();
 		
-		expect(_.toArray(actual)).toEqual([]);
+		expect(actual).toEqual([]);
     });		
 	
 	
@@ -88,35 +90,35 @@ describe('lodash', function () {
     it("keep all even numbers", function(){
         const numbers = [1,2,3,4];
         
-		const actual = _(numbers).filter(isEven);
+		const actual = _(numbers).filter(isEven).value();
 		
-		expect(_.toArray(actual)).toEqual([2,4]);
+		expect(actual).toEqual([2,4]);
     });	
 
 
-	//hint: startsWith	
+	//hint: startsWith
     it("get the full name of all persons where the last name starts with A", function(){
         const persons = [{firstName:"Kalle", lastName:"Anka"},{firstName:"Musse", lastName:"Pigg"},{firstName:"Arne", lastName:"Anka"}];
         
-		const actual = _(persons).filter(person => person.lastName.startsWith("A")).map(person => person.firstName + " " + person.lastName);
+		const actual = _(persons).filter(person => person.lastName.startsWith("A")).map(person => person.firstName + " " + person.lastName).value();
 		
-		expect(_.toArray(actual)).toEqual(["Kalle Anka","Arne Anka"]);
+		expect(actual).toEqual(["Kalle Anka","Arne Anka"]);
     });		
-	
 	
     it("get the full name of all persons where the last name starts with E", function(){
         const persons = [{firstName:"Kalle", lastName:"Anka"},{firstName:"Musse", lastName:"Pigg"},{firstName:"Arne", lastName:"Anka"}];
         
-		const actual = _(persons).filter(person => person.lastName.startsWith("E")).map(person => person.firstName + " " + person.lastName);
+		const actual = _(persons).filter(person => person.lastName.startsWith("E")).map(person => person.firstName + " " + person.lastName).value();
 		
-		expect(_.toArray(actual)).toEqual([]);
+		expect(actual).toEqual([]);
     });		
 	
 	
 	
 	
 	
-	//reduce
+	/*************************************reduce***************************************/
+	
     it("build an apple tree", function(){
         const apples = [];
 		const originalAppleTree = { apples: 0, branches:7, trunk:1, leafs:1000};
@@ -167,6 +169,7 @@ describe('lodash', function () {
 	
 	
 	
+	/************************************More about lodash***********************/
 
 	
     //map also works on string
@@ -180,26 +183,20 @@ describe('lodash', function () {
 	//map also works on object
     it("map attribute + attribute of {one:1, two:'hello'} to equal [2, 'hellohello']", function(){
         const object = {one:1, two:"hello"};
-        const actual = _(object).map((attribute,key) => {
-            console.log(attribute);
-            console.log(key);
-            return attribute + attribute;
-        });
-		expect(_.toArray(actual)).toEqual([2, "hellohello"]);
+		
+        const actual = _(object).map((attribute,key) => attribute + attribute).value();
+		
+		expect(actual).toEqual([2, "hellohello"]);
     });	
 	
 	//map and filter also works on object
     it("map attribute + attribute of {one:1, two:'hello'} to equal [2, 'hellohello']", function(){
         const object = {one:1, two:"hello"};
-        const actual = _(object).filter((attribute, key) => key === "one").map(attribute => attribute + attribute);
-		expect(_.toArray(actual)).toEqual([2]);
+		
+        const actual = _(object).filter((attribute, key) => key === "one").map(attribute => attribute + attribute).value();
+		
+		expect(actual).toEqual([2]);
     });
-	
-	
-	
-
-
-	
     
     
     const persons = [
@@ -216,160 +213,275 @@ describe('lodash', function () {
             {id:11, firstName:"Håkan", lastName: "Andersson", age: 6, childs: [], parents:[7,8], spouses: []}
         ];
         
-    const isParent = person => person.childs.length > 0;    
-	
-	const isChild = person => person.parents.length > 0;
-	
-	const renderPerson = person => `<tr><td>${person.firstName}</td><td>${person.lastName}</td><td>${person.age}</td></tr>`;
-	
-	const isFamily = (person1, person2) => person1.childs.concat(person1.spouses).concat(person1.parents).include(person2.id);
 	
 	const fullName = person => person.firstName + " " + person.lastName;
 	
-	const not = fun => (...args) => !fun(...args);
-	
-	const olderThen = age => person => person.age > age;
-	
-	const youngerThen = age => person => person.age < age;
-	
-	const or = (fun1, fun2) => (...args) => fun1(...args) || fun2(...args);
-	
-	const and = (fun1, fun2) => (...args) => fun1(...args) && fun2(...args);
+	const isParent = person => person.childs.length > 0;  
         
-    //hint: filter and map
     it("find the first name of all parents", function(){
-        const parents = _(persons).filter(isParent).map(fullName);
-        expect(_.toArray(parents)).toEqual([ 'Kalle Anka', 'Rudolf Andersson', 'Karin Andersson' ]);
+        const parents = _(persons).filter(isParent).map(fullName).value();
+        
+		expect(parents).toEqual([ 'Kalle Anka', 'Rudolf Andersson', 'Karin Andersson' ]);
     });
+	
+	
+	/***********************************join********************************/
+	
+    //join 
+    it("join", function(){
+        const actaul = _([1,2,3,4]).join(";");
+        
+		expect(actaul).toEqual('1;2;3;4');
+    });	
+	
+	const renderPerson = person => `<tr><td>${person.firstName}</td><td>${person.lastName}</td><td>${person.age}</td></tr>`;
     
-    //hint: join 
     it("show information about parents in a table", function(){
         const parents = _(persons)
                 .filter(isParent)
                 .map(renderPerson)
                 .join("");
-        expect(parents).toEqual('<tr><td>Kalle</td><td>Anka</td><td>43</td></tr><tr><td>Rudolf</td><td>Andersson</td><td>44</td></tr><tr><td>Karin</td><td>Andersson</td><td>42</td></tr>');
+        
+		expect(parents).toEqual('<tr><td>Kalle</td><td>Anka</td><td>43</td></tr><tr><td>Rudolf</td><td>Andersson</td><td>44</td></tr><tr><td>Karin</td><td>Andersson</td><td>42</td></tr>');
     });
 	
-	//hint: sortBy 
+	
+	
+	/**********************************sortBy**************************************/
+	
+	
+    it("sortBy", function(){
+        const actaul = _([4,2,3,1]).sortBy().value();
+        
+		expect(actaul).toEqual([1,2,3,4]);
+    });	
+	
+	
     it("show information about parents in a table sorted by age", function(){
         const parents = _(persons)
                 .filter(isParent)
 				.sortBy("age")
                 .map(renderPerson)
                 .join("");
-        expect(parents).toEqual('<tr><td>Karin</td><td>Andersson</td><td>42</td></tr><tr><td>Kalle</td><td>Anka</td><td>43</td></tr><tr><td>Rudolf</td><td>Andersson</td><td>44</td></tr>');
+        
+		expect(parents).toEqual('<tr><td>Karin</td><td>Andersson</td><td>42</td></tr><tr><td>Kalle</td><td>Anka</td><td>43</td></tr><tr><td>Rudolf</td><td>Andersson</td><td>44</td></tr>');
     });	
 	
-	//hint: orderBy, first, parseInt
+	/***********************************orderBy & first**************************************/
+	
+    it("orderBy", function(){
+        const actaul = _([{value: 1},{value: 2},{value: 3},{value: 4}]).orderBy('value', 'desc').value();
+        
+		expect(actaul).toEqual([{value: 4},{value: 3},{value: 2},{value: 1}]);
+    });		
+	
+    it("first", function(){
+        const actaul = _([{value: 1},{value: 2},{value: 3},{value: 4}]).first();
+        
+		expect(actaul).toEqual({value: 1});
+    });		
+	
     it("name of oldest person", function(){
         const oldestPerson = _(persons)
 				.orderBy('age','desc')
 				.map(fullName)
                 .first();
-        expect(oldestPerson).toEqual('Musse Pigg');
+        
+		expect(oldestPerson).toEqual('Musse Pigg');
     });	
+	
+	
+	/**************************************size***********************************************/
+	
+    it("size", function(){
+        const actaul = _([1,2,3,4]).size();
+        
+		expect(actaul).toEqual(4);
+    });		
 
-	//hint: size
+	const isChild = person => person.parents.length > 0;	
+	
+	//hint: filter, size
     it("number of childrens", function(){
         const numberOfChildren = _(persons).filter(isChild).size();	
-        expect(numberOfChildren).toEqual(6);
+        
+		expect(numberOfChildren).toEqual(6);
     });
+	
+	
+	
+	/********************************not*****************************************************/
+	
+	const not = fun => (...args) => !fun(...args);
+	
+    it("not", function(){
+		const isBlack = color => color === 'black';
+        
+		expect(isBlack('black')).toEqual(true);
+		expect(not(isBlack)('black')).toEqual(false);
+    });		
+	
+	const olderThen = age => person => person.age > age;
+	
+    it("olderThen", function(){
+		const olderThen30 = olderThen(30);
+        
+		expect(olderThen30({age: 30})).toEqual(false);
+		expect(olderThen30({age: 31})).toEqual(true);
+    });		
+	
 
 	//try to use not and olderThen
-	//Example: isBlack = color => color === 'black'
-	//isBlack('black') === true
-	//not(isBlack)('black') === false
-	
-	//Eaxample
-	//olderThen(30)(45) === true
-	//olderThen(30)(15) === false
     it("name of first person older than 30 in the list that does not have any children", function(){
         const actual = _(persons)
 			.filter(olderThen(30))
 			.filter(not(isParent))
 			.map(fullName)
 			.first();	
-        expect(actual).toEqual('Musse Pigg');
-    });  
+        
+		expect(actual).toEqual('Musse Pigg');
+    }); 
 
-	//hint: every
+	/************************************every*****************************/
+
+    it("every", function(){  
+		expect(_([1,2,3,4]).every(value => value < 5)).toEqual(true);
+		expect(_([1,2,3,4]).every(value => value < 4)).toEqual(false);
+    });	
+	
     it("is all personons older then 7?", function(){
         const actual = _(persons)
 			.every(olderThen(7));	
-        expect(actual).toEqual(false);
+        
+		expect(actual).toEqual(false);
     });  
 	
     it("is all personons older then 3?", function(){
         const actual = _(persons)
 			.every(olderThen(3));	
-        expect(actual).toEqual(true);
+        
+		expect(actual).toEqual(true);
     });
+	
+	/****************************groupBy******************************/
+	
+    it("groupBy", function(){
+        const actual = _([1,2,3,2])
+			.groupBy()
+			.value();
+		
+		const expected = {
+			1: [1],
+			2: [2,2],
+			3: [3],
+		}
+        
+		expect(actual).toEqual(expected);
+    });		
+	
+    it("groupBy", function(){
+        const actual = _([])
+			.groupBy()
+			.value();
+		
+		const expected = {}
+        
+		expect(actual).toEqual(expected);
+    });			
 
-	//hint: groupBy
+
     it("group all persons together on last name", function(){
         const actual = _(persons)
 			.groupBy('lastName')
-			.map(group => group.map(fullName));
+			.map(group => group.map(fullName)).value();
 		
 		const expected = [
 			['Kalle Anka', 'Knatte Anka', 'Fnatte Anka', 'Tjatte Anka'],
 			[ 'Musse Pigg', 'Mimmie Pigg' ],
 			[ 'Sune Andersson', 'Rudolf Andersson', 'Karin Andersson', 'Anna Andersson', 'Håkan Andersson' ]
 		]
-        expect(_.toArray(actual)).toEqual(expected);
+        
+		expect(actual).toEqual(expected);
     });		
 	
-	//hint: groupBy
+
     it("group all persons together on last name, only return the youngest person", function(){
         const actual = _(persons)
 			.groupBy('lastName')
-			.map(group => _(group).orderBy('age').map(fullName).first());
+			.map(group => _(group).orderBy('age').map(fullName).first()).value();
 		
 		const expected = ['Knatte Anka','Mimmie Pigg','Håkan Andersson'];
-        expect(_.toArray(actual)).toEqual(expected);
+        
+		expect(actual).toEqual(expected);
     });	
 
-	const parentOrChild = or(isChild,isParent);
 
-	//hint: partition
+	
+	
+	/********************************partition**********************************/
+	
+	const or = (fun1, fun2) => (...args) => fun1(...args) || fun2(...args);
+	
+	const parentOrChild = or(isChild,isParent);
+	
+    it("partition", function(){
+        const actual = _([1,2,3,2])
+			.partition(isEven)
+			.value();
+		
+		const expected = [[2,2],[1,3]]
+        
+		expect(actual).toEqual(expected);
+    });		
+	
+    it("partition", function(){
+        const actual = _([])
+			.partition(isEven)
+			.value();
+		
+		const expected = [[],[]];
+        
+		expect(actual).toEqual(expected);
+    });		
+	
     it("partition the persons in children and parents", function(){
         const actual = _(persons)
 			.filter(parentOrChild)
 			.partition(isParent)
-			.map(partition => partition.map(fullName))
+			.map(partition => partition.map(fullName)).value();
 		
 		const expected = [
 			[ 'Kalle Anka', 'Rudolf Andersson', 'Karin Andersson' ],
 			[ 'Knatte Anka', 'Fnatte Anka', 'Tjatte Anka', 'Sune Andersson', 'Anna Andersson', 'Håkan Andersson' ]
 		];
-        expect(_.toArray(actual)).toEqual(expected);
+        expect(actual).toEqual(expected);
     });	
 
-	//hint: partition
     it("find youngest child and parent", function(){
         const actual = _(persons)
 			.filter(or(isChild,isParent))
 			.partition(isParent)
-			.map(partition => _(partition).orderBy('age').map(fullName).first())
+			.map(partition => _(partition).orderBy('age').map(fullName).first()).value();
 		
 		const expected = ['Karin Andersson' ,'Håkan Andersson' ];
 		
-        expect(_.toArray(actual)).toEqual(expected);
+        expect(actual).toEqual(expected);
     });	
+	
+	/**********************************permutations***********************/
 
 	const permutations = array => _(array).flatMap(v1 => array.map(v2 => [v1,v2]))
 	
-    it("find all permutations of [0,1]", function(){
-        const actual = permutations([0,1])
+    it("permutations", function(){
+        const actual = permutations([0,1]).value();
+		
 		const expected = [[0,0],[0,1],[1,0],[1,1]];
 		
-        expect(_.toArray(actual)).toEqual(expected);
+        expect(actual).toEqual(expected);
     });	
 	
 	const ageDifference = (person1,person2) => Math.abs(person1.age-person2.age); 
 
-	//hint: use permutations
     it("find ", function(){
         const actual = permutations(persons)
 			.filter(pair => pair[0].id !== pair[1].id)
@@ -379,14 +491,14 @@ describe('lodash', function () {
 					person2: fullName(pair[1]), 
 					difference: ageDifference(pair[0], pair[1])
 				}
-			}).filter(persons => persons.difference > 6 && persons.difference < 10);
+			}).filter(persons => persons.difference > 6 && persons.difference < 10).value();
 		
 		const expected = [
 			{ person1: 'Anna Andersson', person2: 'Håkan Andersson', difference: 7 },
 			{ person1: 'Håkan Andersson', person2: 'Anna Andersson', difference: 7 }
 		];
 		
-        expect(_.toArray(actual)).toEqual(expected);
+        expect(actual).toEqual(expected);
     });	
 	
 	
